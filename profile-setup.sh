b540d68c407506f970eab780d14d5a76d3ce17b2
Ben Martin <monkeyiq@users.sourceforge.net>
2015-01-28 14:57:19 +1000

    .

---
+++ profile-setup.sh
@@ -0,0 +1,6 @@
+
+MoveToCompiler gcc_2.96
+export CFLAGS=" -O0 -g -pg -a "
+export CXXFLAGS=" -O0 -g -pg -a "
+export LDFLAGS=" -pg "
+
---
+++ profileconfigure.sh
@@ -0,0 +1,26 @@
+echo "This is the monkeyiq config invoker. please don't use it."
+
+nice ./configure                      \
+--enable-debug                        \
+--enable-profile                      \
+--with-gnome-vfs-mime                 \
+--enable-djvu                         \
+--with-pdftotext                      \
+--with-dbxml-prefix=/usr              \
+--with-dbxml-db4-prefix=/usr          \
+--enable-stlport-nocustomio           \
+--enable-postgresql-tsearch2-indexing \
+--with-swig-ocaml 
+
+
+# --with-dbxml-prefix=/usr/local/dbxml  \
+# --with-dbxml-db4-prefix=/usr/local/dbxml/db4 
+
+# --with-swig-perl \
+# --with-swig-python-version=python2.2  \
+# --with-swig-python-prefix=/usr        \
+
+
+#--with-libmpeg3=/usr/local/src/libmpeg3-1.3 \
+#--disable-fast-install
+# --with-xml4c=/store/Lindev/xerces-c-src1_5_0 
