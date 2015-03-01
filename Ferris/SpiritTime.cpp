936a4859d6276ca99cc72251a579a18019d546ed
Ben Martin <monkeyiq@users.sourceforge.net>
2015-03-01 21:33:09 +1000

    that boost 1.57 and clang issue... maybe with boost 1.58 things will magically work again? Otherwise, I'll have to special case the cflags for SpiritTime.cpp

--- Ferris/SpiritTime.cpp
+++ Ferris/SpiritTime.cpp
@@ -633,7 +633,7 @@ namespace Ferris
             /********/
             /********/
 
-#ifdef DISABLE_SPIRIT_TIME_ON_PLATFORM_OSX
+#ifdef PLATFORM_OSX
             time_t operator()( const std::string& s, time_t relv  )
                 {
                     cerr << "WARN: SpiritTime is not working on osx yet" << endl;
