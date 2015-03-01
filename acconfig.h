bb1ca7330c57144cdadc281550ec204163cd5809
Ben Martin <monkeyiq@users.sourceforge.net>
2015-03-01 10:51:11 +1000

    adding this for testing

---
+++ acconfig.h
@@ -0,0 +1,169 @@
+
+/* Define if you need to in order for stat and other things to work.  */
+#undef _POSIX_SOURCE
+
+/* Define if you have the ANSI C header files.  */
+#undef STDC_HEADERS
+#undef MIMETYPE_DIRECTORY
+
+#undef HAVE_LIBRPM
+
+#undef FERRIS_AS_TEXT_PLUGIN_DIR
+#undef FERRIS_CREATION_PLUGIN_DIR
+#undef LIBASTEXT_PLUGIN_FACTORY_PREFIX
+#undef FERRIS_HAVE_LIBZ
+#undef FERRIS_HAVE_BZIP2
+#undef FERRIS_HAVE_DB4
+#undef FERRIS_HAVE_GDBM
+#undef FERRIS_HAVE_TDB
+#undef FERRIS_HAVE_EET
+#undef HAVE_EDB
+#undef HAVE_LIBJPEG
+#undef HAVE_LIBPNG
+#undef HAVE_MAGICK
+#undef HAVE_LIBPQXX
+
+#undef FERRIS_USER_NAME_NOBODY
+#undef FERRIS_DEBUG_VM 
+#undef FERRIS_DEBUG_RESOLVE
+
+
+#undef FERRIS_HAVE_ATTR
+#undef FERRIS_HAVE_LIBCAP
+#undef HAVE_DVDREAD
+#undef HAVE_XALAN
+#undef HAVE_XML4C
+#undef HAVE_PATHAN
+
+#undef PREFIX
+
+#undef FERRIS_HAVE_XFSPROGS
+
+#undef FERRIS_HAVE_IPC_CONTEXT
+
+#undef HAVE_GNOMEVFS
+#undef HAVE_LIBFILE
+#undef HAVE_EFSD
+#undef QT_THREAD_SUPPORT
+#undef HAVE_KDE3
+
+/* Define if you have the <dlfcn.h> header file.  */
+#undef HAVE_DLFCN_H
+
+/* Define if you have the crypto library (-lcrypto).  */
+#undef HAVE_LIBCRYPTO
+
+/* Define if you have the fam library (-lfam).  */
+#undef HAVE_LIBFAM
+
+/* Define if you have the file library (-lfile).  */
+#undef HAVE_LIBFILE
+
+/* Name of package */
+#undef PACKAGE
+
+/* Version number of package */
+#undef VERSION
+#undef FERRIS_VERSION
+
+
+#undef HAVE_BOGOFILTER
+#undef HAVE_LIBTEXTCAT
+#undef HAVE_SVMLIGHT
+
+#undef HAVE_DBXML
+
+#undef HAVE_REDLAND
+#undef FERRIS_HAVE_GPGME
+
+#undef HAVE_EPEG
+#undef HAVE_LIBEXIF
+
+#define XFS_SUPER_MAGIC 0x58465342
+
+#undef HAVE_GCJLUCENE
+#undef FERRIS_HAVE_GCJ
+
+
+#undef HAVE_DTL
+#undef HAVE_SQLPLUS
+
+
+#undef HAVE_FCA
+#undef BUILD_FCA
+
+#undef HAVE_LIBSELINUX
+
+#undef HAVE_LIBMAGIC
+#undef BUILD_PGTSEARCH_FTXIDX
+
+
+#undef CURL_LEAVES_SIG_ALARM_ON
+#undef FERRIS_DEBUG_RESOLVE
+#undef FERRIS_DEBUG_VM
+#undef FERRIS_HAVE_GCJ
+#undef FERRIS_HAVE_GPGME
+#undef FERRIS_HAVE_SQLPLUS
+#undef FERRIS_HAVE_XFS
+#undef GCC_HASCLASSVISIBILITY
+#undef HAVE_BOGOFILTER
+#undef HAVE_BONOBO
+#undef HAVE_BOOST
+#undef HAVE_DTL
+#undef HAVE_EET
+#undef HAVE_FAMPP2
+#undef HAVE_GDBM
+#undef HAVE_GPGME
+#undef HAVE_IMLIB2
+#undef HAVE_LDAP
+#undef HAVE_LIBDJVULIBRE
+#undef HAVE_LIBJASPER
+#undef HAVE_LIBPQXX
+#undef HAVE_LOKI
+#undef HAVE_OPENSSL
+#undef HAVE_PATHAN
+#undef HAVE_SIGC
+#undef HAVE_SOCKETPP
+#undef HAVE_STLPORT
+#undef HAVE_STREAMS
+#undef HAVE_TDB
+#undef HAVE_XALAN
+#undef HAVE_XAPIAN
+#undef HAVE_XERCESC
+#undef HAVE_XML4C
+#undef SIGCXX_HAS_LESS_THAN_FIVE_SIGNAL_ARGS_AS_MAX
+#undef STLPORT_IOSIZE
+
+
+#undef FERRIS_DEBUG_RESOLVE
+#undef FERRIS_DEBUG_VM
+#undef FERRIS_HAVE_GCJ
+#undef FERRIS_HAVE_GPGME
+#undef HAVE_BOGOFILTER
+#undef HAVE_BOOST
+#undef HAVE_DTL
+#undef HAVE_FAMPP2
+#undef HAVE_GPGME
+#undef HAVE_LIBPQXX
+#undef HAVE_LOKI
+#undef HAVE_PATHAN
+#undef HAVE_SIGC
+#undef HAVE_SOCKETPP
+#undef HAVE_STLPORT
+#undef HAVE_STREAMS
+#undef HAVE_XALAN
+#undef HAVE_XERCESC
+#undef HAVE_XML4C
+#undef STLPORT_IOSIZE
+
+#undef FERRIS_HAVE_PLASMA
+#undef FERRIS_HAVE_QPRINTER
+#undef FERRIS_HAVE_SANE
+#undef HAVE_HEVEA
+#undef HAVE_KDE
+#undef HAVE_PLASMA
+#undef HAVE_PS2ASCII
+#undef HAVE_QPRINTER
+#undef HAVE_SANE
+#undef HAVE_UNRTF
+#undef FERRIS_HAVE_KDE
