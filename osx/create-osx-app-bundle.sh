1589103568b60319df1122486744d695b9572d82
Ben Martin <monkeyiq@users.sourceforge.net>
2015-07-17 16:57:01 +1000

    A little more for the osx bundle work

--- osx/create-osx-app-bundle.sh
+++ osx/create-osx-app-bundle.sh
@@ -93,9 +93,21 @@ LANG=C sed -i -e 's|/Users/ben/macports-personal/share/qt4/plugins|/Applications
 
 rsync -av /Users/ben/macports-personal/lib/libsqlite* $bundle_lib/
 rsync -av /Users/ben/macports-personal/lib/libintl*   $bundle_lib/
+cd /Users/ben/macports-personal/lib/
+rsync -av librdf*  librasqal* libraptor2* db46 libltdl* libmhash* libmpfr* libgmp* libxml2* $bundle_lib/
 cd $bundle_lib/
 library-paths-macports-personal-to-absolute.sh libsqlite3.dylib  /Applications/Ferris.app/Contents/Resources/usr/local/lib
 library-paths-macports-personal-to-absolute.sh libintl.dylib  /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh librdf.dylib  /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh librasqal.dylib /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh libraptor2.dylib /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh libxml2.dylib /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh db46/libdb-4.6.dylib  /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh libltdl.dylib  /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh libmhash.dylib /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh libmpfr.dylib  /Applications/Ferris.app/Contents/Resources/usr/local/lib
+library-paths-macports-personal-to-absolute.sh libgmp.dylib   /Applications/Ferris.app/Contents/Resources/usr/local/lib
+ln -s db46/libdb-4.6.dylib .
 ln -s /Applications/Ferris.app/Contents/Frameworks/QtSql.framework/Versions/4/QtSql .
 ln -s /Applications/Ferris.app/Contents/Frameworks/QtCore.framework/Versions/4/QtCore .
 ln -s /Applications/Ferris.app/Contents/Frameworks/QtXml.framework/Versions/4/QtXml .
@@ -103,10 +115,17 @@ ln -s /Applications/Ferris.app/Contents/Frameworks/QtXml.framework/Versions/4/Qt
 
 ##install_name_tool -change /Users/ben/macports-personal/Library/Frameworks/QtCore.framework/Versions/4/QtCore /Applications/Ferris.app/Contents/Frameworks/QtSql.framework/Versions/4/QtSql  QtSql 
 
-for libname in QtNetwork QtXml QtDBus
+for libname in QtNetwork QtXml QtDBus QtCore
 do
     ls -l /Applications/Ferris.app/Contents/Frameworks/$libname.framework/Versions/4/$libname
 library-paths-macports-personal-to-absolute.sh /Applications/Ferris.app/Contents/Frameworks/$libname.framework/Versions/4/$libname /Applications/Ferris.app/Contents/Resources/usr/local/lib
 done
 
 ###library-paths-macports-personal-to-absolute.sh /Applications/Ferris.app/Contents/Frameworks/QtXml.framework/Versions/4/QtXml  /Applications/Ferris.app/Contents/Resources/usr/local/lib
+
+rsync -av /Users/ben/macports-personal/share/soprano $bundle_res/usr/share/
+rsync -av /Users/ben/macports-personal/lib/soprano   $bundle_res/usr/local/lib/
+
+rsync -av /Users/ben/macports-personal/libexec/gam_server $bundle_res/usr/local/bin
+cd $bundle_res/usr/local/bin
+library-paths-macports-personal-to-absolute.sh gam_server /Applications/Ferris.app/Contents/Resources/usr/local/lib
